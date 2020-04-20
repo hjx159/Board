e636f6af928ccaa91d849352ec0173daffdd4016
hjx159 <812143280@qq.com>
2020-04-20 13:26:23 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <graphics.h>	//包含EGE的头文件
+#include<iostream>
+#include<vector>
+#include"CCircle.h"
+#include"CColor.h"
+#include"CPoint.h"
+#include"CUi.h"
+#include"CRect.h"
+#include"CMenu.h"
+#include"CPoly.h"
+#include"CClearScreen.h"
+
+int main()
+{
+	CUi ui{};//初始化画布
+	CMenu menu{};//通过无参构造函数进入菜单
+	return 0;
+}
